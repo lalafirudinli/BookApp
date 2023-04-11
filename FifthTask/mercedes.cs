e8b1398c46541e884c95bedd7ae2bd5508e90d09
lalafirudinli <129064225+lalafirudinli@users.noreply.github.com>
2023-04-11 05:18:00 +0400

    Update mercedes.cs

--- FifthTask/mercedes.cs
+++ FifthTask/mercedes.cs
@@ -1,1 +1,22 @@
 
+public class Mercedes : Car
+{
+    private bool isamg = true;
+
+    public bool İsAmg
+    {
+        get
+        {
+            return isamg;
+        }
+        set {
+            isamg = value;
+        }
+    }
+
+    public Mercedes(string model, int topspeed, double engine, double km, int hp) : base(model, topspeed, engine, km, hp)
+    {
+    }
+
+}
+
