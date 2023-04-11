4493c6bf49f9cb21a358b5960f2a42e679548969
lalafirudinli <129064225+lalafirudinli@users.noreply.github.com>
2023-04-11 05:17:31 +0400

    Update bmw.cs

--- FifthTask/bmw.cs
+++ FifthTask/bmw.cs
@@ -1,1 +1,24 @@
 
+internal class BMW : Car
+{
+    private bool isPower = true;
+
+    public bool Ispower
+    {
+        get
+        {
+            return isPower;
+        }
+        set
+        {
+            isPower = value;
+        }
+    }
+
+    public BMW(string model, int topspeed, double engine, double km, int hp) : base(model, topspeed, engine, km, hp)
+    {
+    }
+
+
+}
+
