6f244d68a10d190523394fa9389c15e2b8408f2c
lalafirudinli <129064225+lalafirudinli@users.noreply.github.com>
2023-04-23 23:42:25 +0400

    Update program.cs

--- CustomList/program.cs
+++ CustomList/program.cs
@@ -1,1 +1,28 @@
 
+class MyCustomList<T> where T: class
+{
+    private T[] List;
+
+    public MyCustomList()
+    {
+        List = new T[0];
+    }
+    
+    public void Add(T item)
+    {
+        Array.Resize(ref List, List.Length + 1);
+        List[List.Length - 1] = item;
+    }
+
+    public T Find(Predicate<T> func)
+    {
+        foreach(T item in List)
+        {
+            if (func(item))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
